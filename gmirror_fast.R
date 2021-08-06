049077c9ce7e8f7ffa9f95dadbd0a34e7f6c6661
Jianhui Gao <58570421+jianhuig@users.noreply.github.com>
2021-08-06 15:40:25 -0400

    Create gmirror_fast.R

---
+++ gmirror_fast.R
@@ -0,0 +1,235 @@
+#' gmirror
+#'
+#' Create mirrored Manhattan plots for GWAS
+#' Dependencies: ggplot2, gridExtra
+#' Suggested: RColorBrewer, ggrepel
+#' @param top data frame, if not plato or plink format, must contain SNP, CHR, POS, pvalue, optional Shape
+#' @param bottom data frame, if not plato or plink format, must contain SNP, CHR, POS, pvalue, optional Shape
+#' @param tline list of pvalues to draw red threshold lines in top plot
+#' @param bline ist of pvalues to draw red threshold lines in bottom plot
+#' @param chroms list of chromosomes to plot in the order desired, default c(1:22, "X", "Y")
+#' @param log10 plot -log10() of pvalue column, boolean
+#' @param yaxis label for y-axis in the format c("top", "bottom"), automatically set if log10=TRUE
+#' @param opacity opacity of points, from 0 to 1, useful for dense plots
+#' @param annotate_snp vector of RSIDs to annotate
+#' @param annotate_p list of pvalue thresholds to annotate in the order of c(p_top, p_bottom)
+#' @param toptitle optional string for top plot title
+#' @param bottomtitle optional string for bottom plot title
+#' @param chrcolor1 first alternating color for chromosome
+#' @param chrcolor2 second alternating color for chromosome
+#' @param highlight_snp vector of snps to highlight
+#' @param highlight_p list of pvalue thresholds to highlight in the order of c(p_top, p_bottom)
+#' @param highlighter color to highlight
+#' @param freey allow y-axes to scale with the data
+#' @param background variegated or white
+#' @param chrblocks boolean, turns on x-axis chromosome marker blocks
+#' @param file file name of saved image
+#' @param hgt height of plot in inches
+#' @param hgtratio height ratio of plots, equal to top plot proportion
+#' @param wi width of plot in inches
+#' @param res resolution of plot in pixels per inch
+#' @return png image
+#' @import ggplot2
+#' @importFrom gridExtra arrangeGrob grid.arrange
+#' @export
+#' @examples
+#' data(gwas.t)
+#' data(gwas.b)
+#' gmirror(top=gwas.t, bottom=gwas.b, tline=0.05/nrow(gwas.t), bline=0.05/nrow(gwas.b), 
+#' toptitle="GWAS Comparison Example: Data 1", bottomtitle = "GWAS Comparison Example: Data 2", 
+#' highlight_p = c(0.05/nrow(gwas.t), 0.05/nrow(gwas.b)), highlighter="green")
+library(ggallin)
+library(gridExtra)
+library(ggplot2)
+library(scattermore)
+
+gmirror <- function(top, bottom, tline, bline, chroms = c(1:22, "X", "Y"),log10=TRUE, 
+                    yaxis, opacity=1, annotate_snp, annotate_p, toptitle=NULL, 
+                    bottomtitle=NULL, highlight_snp, highlight_p, highlighter="red", 
+                    chrcolor1="#AAAAAA", chrcolor2="#4D4D4D", freey=FALSE, 
+                    background="variegated", chrblocks=FALSE, 
+                    file="gmirror", hgt=7, hgtratio=0.5, wi=12, res=300, log_axis = FALSE){
+  
+  #Sort data
+  topn <- names(top)
+  bottomn <- names(bottom)
+  top$Location <- "Top"
+  bottom$Location <- "Bottom"
+  d <- rbind(top, bottom)
+  d$POS <- as.numeric(as.character(d$POS))
+  d$CHR <- droplevels(factor(d$CHR, levels = as.character(chroms)))
+  d <- d[d$CHR %in% chroms, ]
+  d_order <- d[order(d$CHR, d$POS), ]
+  d_order$pos_index <- seq.int(nrow(d_order))
+  d_order_sub <- d_order[, c("SNP", "CHR", "POS", "pvalue", "pos_index")]
+  
+  #Set up dataframe with color and position info
+  maxRows <- by(d_order_sub, d_order_sub$CHR, function(x) x[which.max(x$pos_index),])
+  minRows <- by(d_order_sub, d_order_sub$CHR, function(x) x[which.min(x$pos_index),])
+  milimits <- do.call(rbind, minRows)
+  malimits <- do.call(rbind, maxRows)
+  lims <- merge(milimits, malimits, by="CHR")
+  names(lims) <- c("Color", "snpx", "px", "posx", "posmin", "snpy", "py", "posy", "posmax")
+  lims$av <- (lims$posmin + lims$posmax)/2
+  lims <- lims[order(lims$Color),]
+  lims$shademap <- rep(c("shade_ffffff", "shade_ebebeb"), length.out=nrow(lims), each=1)
+  
+  #Set up colors
+  nchrcolors <- nlevels(factor(lims$Color))
+  
+  #Color by CHR
+  colnames(d_order)[2] <- "Color"
+  newcols <-c(rep(x=c(chrcolor1, chrcolor2), length.out=nchrcolors, each=1), "#FFFFFF", "#EBEBEB")
+  names(newcols) <-c(levels(factor(lims$Color)), "shade_ffffff", "shade_ebebeb")
+  
+  #Info for y-axis
+  if(log10==TRUE){
+    d_order$pval <- -log10(d_order$pvalue)
+    yaxislab1 <- expression(paste("-log"[10], "(p-value)", sep=""))
+    yaxislab2 <- expression(paste("-log"[10], "(p-value)", sep=""))
+    if(!missing(tline)) {tredline <- -log10(tline)}
+    if(!missing(bline)) {bredline <- -log10(bline)}
+  } else {
+    d_order$pval <- d_order$pvalue
+    yaxislab1 <- yaxis[1]
+    yaxislab2 <- yaxis[2]
+    if(!missing(tline)) {tredline <- tline}
+    if(!missing(bline)) {bredline <- bline}
+  }
+  yaxismax1 <- ifelse(freey==FALSE, max(d_order$pval[which(d_order$pval< Inf)]), max(d_order$pval[which(d_order$pval< Inf) & d_order$Location=="Top"]))
+  yaxismax2 <- ifelse(freey==FALSE, max(d_order$pval[which(d_order$pval< Inf)]), max(d_order$pval[which(d_order$pval< Inf) & d_order$Location=="Bottom"]))
+  yaxismin1 <- ifelse(freey==FALSE, 0, min(d_order$pval[d_order$Location=="Top"]))
+  yaxismin2 <- ifelse(freey==FALSE, 0, min(d_order$pval[d_order$Location=="Bottom"]))
+  
+  #Theme options
+  backpanel1 <- ifelse(background=="white", "NULL", "geom_rect(data = lims, aes(xmin = posmin-.5, xmax = posmax+.5, ymin = yaxismin1, ymax = Inf, fill=factor(shademap)), alpha = 0.5)" )
+  backpanel2 <- ifelse(background=="white", "NULL", "geom_rect(data = lims, aes(xmin = posmin-.5, xmax = posmax+.5, ymin = yaxismin2, ymax = Inf, fill=factor(shademap)), alpha = 0.5)" )
+  
+  #Start plotting
+  #TOP PLOT
+  p1 <- ggplot() + eval(parse(text=backpanel1))
+  #Add shape info if available
+  if("Shape" %in% topn){
+    p1 <- p1 + geom_point(data=d_order[d_order$Location=="Top",], aes(x=pos_index, y=pval, color=factor(Color), shape=factor(Shape)), alpha=opacity)
+  } else {
+    p1 <- p1 + geom_scattermore(data=d_order[d_order$Location=="Top",], aes(x=pos_index, y=pval, color=factor(Color)), alpha=opacity)
+  }
+  p1 <- p1 + scale_x_continuous(breaks=lims$av, labels=lims$Color, expand=c(0,0))
+  if(chrblocks==TRUE){
+    p1 <- p1 + geom_rect(data = lims, aes(xmin = posmin-.5, xmax = posmax+.5, ymin = -Inf, ymax = min(d_order$pval), fill=as.factor(Color)), alpha = 1)
+  }
+  p1 <- p1 + scale_colour_manual(name = "Color", values = newcols) + scale_fill_manual(name = "Color", values = newcols)
+  p1 <- p1 + theme(panel.grid.minor.x = element_blank(), panel.grid.major.x=element_blank(), axis.title.x=element_blank(), legend.position="top", legend.title=element_blank())
+  
+  #BOTTOM PLOT
+  p2 <- ggplot() + eval(parse(text=backpanel2))
+  #Add shape info if available
+  if("Shape" %in% bottomn){
+    p2 <- p2 + geom_point(data=d_order[d_order$Location=="Bottom",], aes(x=pos_index, y=pval, color=factor(Color), shape=factor(Shape)), alpha=opacity)
+  } else {
+    p2 <- p2 + geom_scattermore(data=d_order[d_order$Location=="Bottom",], aes(x=pos_index, y=pval, color=factor(Color)), alpha=opacity)
+  }
+  p2 <- p2 + scale_x_continuous(breaks=lims$av, labels=lims$Color, expand=c(0,0))
+  if(chrblocks==TRUE){
+    p2 <- p2 + geom_rect(data = lims, aes(xmin = posmin-.5, xmax = posmax+.5, ymin = -Inf, ymax = min(d_order$pval), fill=as.factor(Color)), alpha = 1)
+  }  
+  p2 <- p2 + scale_colour_manual(name = "Color", values = newcols) + scale_fill_manual(name = "Color", values = newcols)
+  p2 <- p2 + theme(axis.text.x=element_text(angle=90), panel.grid.minor.x = element_blank(), panel.grid.major.x=element_blank(), axis.title.x=element_blank(), legend.position="bottom", legend.title=element_blank())
+  
+  #Highlight if given
+  if(!missing(highlight_snp)){
+    if("Shape" %in% topn){
+      p1 <- p1 + geom_point(data=d_order[d_order$SNP %in% highlight_snp & d_order$Location=="Top", ], aes(x=pos_index, y=pval, shape=Shape), colour=highlighter)
+      p1 <- p1 + guides(shape = guide_legend(override.aes = list(colour = "black")))
+    } else {
+      p1 <- p1 + geom_scattermore(data=d_order[d_order$SNP %in% highlight_snp & d_order$Location=="Top", ], aes(x=pos_index, y=pval), colour='green')
+    }
+    if("Shape" %in% bottomn){
+      p2 <- p2 + geom_point(data=d_order[d_order$SNP %in% highlight_snp & d_order$Location=="Bottom", ], aes(x=pos_index, y=pval, shape=Shape), colour=highlighter)
+      p2 <- p2 + guides(shape = guide_legend(override.aes = list(colour = "black")))
+    } else {
+      p2 <- p2 + geom_scattermore(data=d_order[d_order$SNP %in% highlight_snp & d_order$Location=="Bottom", ], aes(x=pos_index, y=pval), colour=highlighter)
+    }
+  }
+  if(!missing(highlight_p)){
+    if("Shape" %in% topn){
+      p1 <- p1 + geom_point(data=d_order[d_order$pvalue < highlight_p[1] & d_order$Location=="Top", ], aes(x=pos_index, y=pval, shape=Shape), colour=highlighter)
+      p1 <- p1 + guides(shape = guide_legend(override.aes = list(colour = "black")))
+    } else {
+      p1 <- p1 + geom_point(data=d_order[d_order$pvalue < highlight_p[1] & d_order$Location=="Top", ], aes(x=pos_index, y=pval), colour=highlighter)
+    }
+    if("Shape" %in% bottomn){
+      p2 <- p2 + geom_point(data=d_order[d_order$pvalue < highlight_p[2] & d_order$Location=="Bottom", ], aes(x=pos_index, y=pval, shape=Shape), colour=highlighter)
+      p2 <- p2 + guides(shape = guide_legend(override.aes = list(colour = "black")))
+    } else {
+      p2 <- p2 + geom_point(data=d_order[d_order$pvalue < highlight_p[2] & d_order$Location=="Bottom", ], aes(x=pos_index, y=pval), colour=highlighter)
+    }
+  }
+  #Add pvalue threshold line
+  if(!missing(tline)){
+    for(i in 1:length(tline)){
+      p1 <- p1 + geom_hline(yintercept = tredline[i], colour="red")
+    }
+  }
+  if(!missing(bline)){
+    for(i in 1:length(bline)){
+      p2 <- p2 + geom_hline(yintercept = bredline[i], colour="red")
+    }
+  }
+  #Annotate
+  if(!missing(annotate_p)){
+    if (!requireNamespace(c("ggrepel"), quietly = TRUE)==TRUE) {
+      print("Consider installing 'ggrepel' for improved text annotation")
+      p1 <- p1 + geom_text(data=d_order[d_order$pvalue < annotate_p[1] & d_order$Location=="Top",], aes(pos_index,pval,label=SNP))
+      p2 <- p2 + geom_text(data=d_order[d_order$pvalue < annotate_p[2] & d_order$Location=="Bottom",], aes(pos_index,pval,label=SNP))
+    } else {
+      p1 <- p1 + ggrepel::geom_text_repel(data=d_order[d_order$pvalue < annotate_p[1] & d_order$Location=="Top",], aes(pos_index,pval,label=SNP))
+      p2 <- p2 + ggrepel::geom_text_repel(data=d_order[d_order$pvalue < annotate_p[2] & d_order$Location=="Bottom",], aes(pos_index,pval,label=SNP))
+    }
+  }
+  if(!missing(annotate_snp)){
+    if (!requireNamespace(c("ggrepel"), quietly = TRUE)==TRUE){
+      print("Consider installing 'ggrepel' for improved text annotation")
+      p1 <- p1 + geom_text(data=d_order[d_order$SNP %in% annotate_snp & d_order$Location=="Top",], aes(pos_index,pval,label=SNP))
+      p2 <- p2 + geom_text(data=d_order[d_order$SNP %in% annotate_snp & d_order$Location=="Bottom",], aes(pos_index,pval,label=SNP))
+    } else {
+      p1 <- p1 + ggrepel::geom_text_repel(data=d_order[d_order$SNP %in% annotate_snp & d_order$Location=="Top",], aes(pos_index,pval,label=SNP))
+      p2 <- p2 + ggrepel::geom_text_repel(data=d_order[d_order$SNP %in% annotate_snp & d_order$Location=="Bottom",], aes(pos_index,pval,label=SNP))
+    }
+  }
+  #Add title and y axis title
+  p1 <- p1 + ylab(yaxislab1)
+  p2 <- p2 + ylab(yaxislab2)
+  
+  #Format
+  if(chrblocks==TRUE){
+    if(freey==TRUE){
+      print("Sorry, drawing chrblocks with freey=TRUE is currently unsupported and will be ignored.")
+    } else {
+      p1 <- p1+theme(axis.text.x = element_text(vjust=1),axis.ticks.x = element_blank())+ylim(c(yaxismin1,yaxismax1))
+      p2 <- p2+scale_y_reverse(limits=c(yaxismax2, yaxismin2)) + theme(axis.text.x = element_blank(),axis.ticks.x = element_blank())
+    }
+  } else {
+    if(log_axis==TRUE){
+    p1 <- p1+theme(axis.text.x = element_text(vjust=1),axis.ticks.x = element_blank())+ scale_y_continuous(limits=c(yaxismin1, yaxismax1),expand=expansion(mult=c(0,0.1)),trans=scales::pseudo_log_trans(base = 10))
+
+    p2 <- p2+scale_y_continuous(limits=c(yaxismax2,yaxismin2),expand=expansion(mult=c(0.1,0)),trans=pseudo_log_trans(base=10) %of% reverse_trans()) + theme(axis.text.x = element_blank(),axis.ticks.x = element_blank())} else{
+      p1 <- p1+theme(axis.text.x = element_text(vjust=1),axis.ticks.x = element_blank())+ scale_y_continuous(limits=c(yaxismin1, yaxismax1),expand=expansion(mult=c(0,0.1)))
+      
+      p2 <- p2+scale_y_continuous(limits=c(yaxismax2,yaxismin2),expand=expansion(mult=c(0.1,0)),trans=reverse_trans()) + theme(axis.text.x = element_blank(),axis.ticks.x = element_blank())
+      
+    }
+  }
+  
+  if(background=="white"){
+    p1 <- p1 + theme(panel.background = element_rect(fill="white"))
+    p2 <- p2 + theme(panel.background = element_rect(fill="white"))
+  }
+  p1 <- p1 + guides(fill="none", color="none")
+  p2 <- p2 + guides(fill="none", color="none")
+  #Save
+  print(paste("Saving plot to ", file, ".png", sep=""))
+  p <- grid.arrange(arrangeGrob(p1, top=toptitle), arrangeGrob(p2, bottom=bottomtitle), padding=0, heights=c(hgtratio,1-hgtratio))
+  ggsave(p, filename=paste(file, ".png", sep=""), dpi=res, units="in", height=hgt, width=wi)
+  return(p)
+}
